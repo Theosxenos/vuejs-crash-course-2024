a592270245b48e3d7e404a0380f40d5cc548a769
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-07-20 15:08:35 +0200

    Finish composition API example

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
-<script>
-  export default {
-    data() {
-      return {
-        name: 'John Doe',
-        status: 'pending',
-        tasks: ['Task One', 'Task Two', 'Task Three'],
-        link: 'https://github.com',
-      };
-    },
-    methods: {
-      toggleStatus() {
-        if(this.status === 'active') {
-          this.status = 'pending';
-        } 
-        else if(this.status === 'pending') {
-          this.status = 'inactive';
-        }
-        else {
-          this.status = 'active';
-        }
-      },
-    },
+<script setup>
+  import {ref, onMounted} from "vue";
+
+  const name = ref('John Doe');
+  const status = ref('active');
+  const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+  const newTask = ref('');
+
+  const toggleStatus = () => {
+    if (status.value === 'active') {
+      status.value = 'pending';
+    } else if (status.value === 'pending') {
+      status.value = 'inactive';
+    } else {
+      status.value = 'active';
+    }
   };
+  
+  const addTask = () => {
+    if(newTask.value.trim() !== '') {
+      tasks.value.push(newTask.value);
+      newTask.value = '';
+    }
+  };
+  
+  const deleteTask = (index) => {
+    tasks.value.splice(index, 1);
+  }
+  
+  onMounted(async () => {
+    try {
+      const response = await fetch('http://localhost:4000/tasks')
+      const data = await response.json();
+      tasks.value = data.map((t) => t.title);
+    } catch (error) {
+      console.log(`Error fetching tasks: ${error}`);
+    }   
+  });
 </script>
 
 <template>
@@ -31,12 +45,19 @@
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
   
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input id="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+  
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{task}}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{task}}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
-<!--  <a v-bind:href="link">Click for Github</a>-->
-  <a :href="link">Click for Github</a>
   <br/>
 <!--  <button v-on:click="toggleStatus">Change Status</button>-->
   <button @click="toggleStatus">Change Status</button>
