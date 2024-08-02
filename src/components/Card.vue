4df952a65dac4e213a8c27c26bbc740dd34b2659
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-08-02 21:10:58 +0200

    Even more routing

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,12 @@
 <script setup>
-  import {defineProps} from "vue";
-  
-  defineProps({
-    bg: {
-      type: String,
-      default: 'bg-gray-100'
-    },
-  });
+import { defineProps } from "vue";
+
+defineProps({
+  bg: {
+    type: String,
+    default: 'bg-gray-100'
+  },
+});
 </script>
 
 <template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,8 @@
 <script setup>
 import JobListing from "@/components/JobListing.vue";
 import jobData from '@/jobs.json'
-import {ref, defineProps} from "vue";
+import { ref, defineProps } from "vue";
+import {RouterLink} from "vue-router";
 
 defineProps({
   limit: Number,
@@ -22,16 +23,14 @@ console.log(jobs.value)
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.size())" :key="job.id" :job="job"/>
+        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
       </div>
     </div>
   </section>
 
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a
-        href="/jobs/"
-        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-    >View All Jobs</a
-    >
+    <RouterLink to="/jobs/" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+      View All Jobs
+    </RouterLink>
   </section>
 </template>
